b81f043c155ebdc0fa10fedde309dc34d5404124
muxue wei <752005116@qq.com>
2022-03-14 16:20:20 +0800

    图库

--- app/lib/ui/pages/home/gallery.dart
+++ app/lib/ui/pages/home/gallery.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 class GalleryPage extends ConsumerStatefulWidget {
@@ -13,8 +14,48 @@ class GalleryPage extends ConsumerStatefulWidget {
 class _InputPageState extends ConsumerState<GalleryPage> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.red.withAlpha(100),
+    return Scaffold(
+      body: Container(
+        color: Colors.white,
+        child: SafeArea(
+          child: Column(
+            children: [
+              SizedBox(
+                child: Row(
+                  children: [
+                    Container(
+                      width: 20,
+                    ),
+                    Expanded(
+                      child: Container(
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(22),
+                          color: const Color(0xFFEAEAEA),
+                        ),
+                        child: (Row(
+                          children: [
+                            SvgPicture.asset('assets/images/icon_search.svg', height: 24, width: 24),
+                            const SizedBox(width: 10),
+                            const Expanded(child: Text('search'))
+                          ],
+                        )),
+                        padding: EdgeInsets.all(10),
+                      ),
+                    ),
+                    Container(
+                      width: 20,
+                    ),
+                  ],
+                ),
+                height: 44,
+              ),
+              const SizedBox(
+                height: 80,
+              )
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
--- app/lib/ui/pages/home/home.dart
+++ app/lib/ui/pages/home/home.dart
@@ -47,34 +47,33 @@ class _HomeScreenState extends ConsumerState<HomeScreen> {
       bottomNavigationBar: AnimatedSlide(
         duration: Duration(milliseconds: 250),
         offset: Offset(0, ref.watch(bottomBarVisibilityProvider.state).state ? 0 : 1),
-        child: SafeArea(
-          child: Theme(
-            data: Theme.of(context).copyWith(
-              canvasColor: Colors.transparent,
-            ),
-            child: BottomNavigationBar(
-              type: BottomNavigationBarType.fixed,
-              showSelectedLabels: false,
-              showUnselectedLabels: false,
-              elevation: 0,
-              items: const [
-                BottomNavigationBarItem(icon: Icon(Icons.input), label: "input"),
-                BottomNavigationBarItem(icon: Icon(Icons.album), label: "album"),
-                BottomNavigationBarItem(icon: Icon(Icons.explore), label: "explore"),
-                BottomNavigationBarItem(icon: Icon(Icons.person), label: "person"),
-              ],
-              selectedItemColor: Colors.blue,
-              unselectedItemColor: Colors.grey,
-              onTap: (index) {
-                // HomeScreenState state = ref.read(widget.provider);
-                ref.read(widget.pageIndexProvider.state).state = index;
-              },
-              currentIndex: index,
-            ),
-          ).frosted(
-            blur: 2.5,
-            frostColor: Colors.white,
+        child: Theme(
+          data: Theme.of(context).copyWith(
+            canvasColor: Colors.transparent,
           ),
+          child: BottomNavigationBar(
+            type: BottomNavigationBarType.fixed,
+            showSelectedLabels: false,
+            showUnselectedLabels: false,
+            elevation: 0,
+            items: const [
+              BottomNavigationBarItem(icon: Icon(Icons.input), label: "input"),
+              BottomNavigationBarItem(icon: Icon(Icons.image), label: "album"),
+              BottomNavigationBarItem(icon: Icon(Icons.explore), label: "explore"),
+              BottomNavigationBarItem(icon: Icon(Icons.person), label: "person"),
+            ],
+            selectedItemColor: Colors.blue,
+            unselectedItemColor: Colors.grey,
+            onTap: (index) {
+              // HomeScreenState state = ref.read(widget.provider);
+              ref.read(widget.pageIndexProvider.state).state = index;
+            },
+            currentIndex: index,
+          ),
+        ).frosted(
+          blur: 2.5,
+          frostColor: Colors.white,
+          frostOpacity: 0.7,
         ),
       ),
     );
