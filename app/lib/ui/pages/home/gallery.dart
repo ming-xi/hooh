02f771043050786bf8c1cf36783476f6174fc406
xmk <289203664@qq.com>
2022-03-17 11:47:56 +0800

    图库下拉刷新

--- app/lib/ui/pages/home/gallery.dart
+++ app/lib/ui/pages/home/gallery.dart
@@ -6,17 +6,16 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:tuple/tuple.dart';
-final galleryCategoriesProvider =
-    StateProvider((ref) => <GalleryCategoryItem>[]);
+
+final galleryCategoriesProvider = StateProvider((ref) => <GalleryCategoryItem>[]);
 final selectedCategoryProvider = StateProvider((ref) {
-  return ref
-      .watch(galleryCategoriesProvider.state)
-      .state
-      .where((element) => element.selected)
-      .first;
+  return ref.watch(galleryCategoriesProvider.state).state.firstWhere(
+        (element) => element.selected,
+        orElse: () => GalleryCategoryItem(null, false),
+      );
 });
-final galleryImagesProvider = FutureProvider.autoDispose
-    .family<List<GalleryImage>, Tuple2<String, int>>((ref, tuple) async {
+final galleryImagesProvider = StateProvider((ref) => <GalleryImage>[]);
+final newGalleryImagesProvider = FutureProvider.autoDispose.family<List<GalleryImage>, Tuple2<String, int>>((ref, tuple) async {
   // // Cancel the HTTP request if the user leaves the detail page before
   // // the request completes.
   // final cancelToken = CancelToken();
@@ -53,33 +52,78 @@ class _GalleryPageState extends ConsumerState<GalleryPage> {
 
   @override
   Widget build(BuildContext context) {
-    final ScrollController _controller = new ScrollController();
-    List<GalleryCategoryItem> categories =
-        ref.watch(galleryCategoriesProvider.state).state;
-
-    var safeId =
-        ref.watch(selectedCategoryProvider.state).state.galleryCategory.safeId;
-    var width = MediaQuery.of(context).size.width~/3;
-    var listWidget = ref.watch(galleryImagesProvider(Tuple2(safeId,width))).when(
-        data: (data) => GridView.builder(
-              padding: const EdgeInsets.fromLTRB(16, 0, 10, 0),
-              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                  //横轴元素个数
-                  crossAxisCount: 3,
-                  // //纵轴间距
-                  // mainAxisSpacing: 0.0,
-                  // //横轴间距
-                  // crossAxisSpacing: 0.0,
-                  //子组件宽高长度比例
-                  childAspectRatio: 1.0),
-              itemCount: data.length,
-              itemBuilder: (BuildContext context, int index) =>
-                  GalleryImageView(data[index]),
-            ),
-        error: (error, stack) => Container(
-              color: Colors.red,
-            ),
-        loading: () => const CircularProgressIndicator());
+    debugPrint("build page");
+    final ScrollController _controller = ScrollController();
+    List<GalleryCategoryItem> categories = ref.watch(galleryCategoriesProvider.state).state;
+    List<GalleryImage> images = ref.watch(galleryImagesProvider.state).state;
+    String? safeId = ref.watch(selectedCategoryProvider.state).state.galleryCategory?.safeId;
+    int width = MediaQuery.of(context).size.width ~/ 3;
+    Widget listWidget;
+    if (safeId == null) {
+      listWidget = Container();
+    } else {
+      // 之前的做法，先定义refreshIndicator，在里面调用了images
+      // var refreshIndicator = RefreshIndicator(
+      //   onRefresh: () async {
+      //     ref.read(galleryImagesProvider.state).state = [];
+      //     ref.read(galleryImagesPageProvider.state).state = 1;
+      //     return ref.refresh(newGalleryImagesProvider(Tuple2(safeId, width)));
+      //   },
+      //   child: GridView.builder(
+      //     padding: const EdgeInsets.fromLTRB(16, 0, 10, 0),
+      //     gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+      //         //横轴元素个数
+      //         crossAxisCount: 3,
+      //         // //纵轴间距
+      //         // mainAxisSpacing: 0.0,
+      //         // //横轴间距
+      //         // crossAxisSpacing: 0.0,
+      //         //子组件宽高长度比例
+      //         childAspectRatio: 1.0),
+      //     itemCount: images.length,
+      //     itemBuilder: (BuildContext context, int index) => GalleryImageView(images[index]),
+      //   ),
+      // );
+      // 但是这里在闭包里又修改了images的值。可能导致了前面的refreshIndicator无法正确刷新状态
+      // listWidget = ref.watch(newGalleryImagesProvider(Tuple2(safeId, width))).when(
+      //     data: (data) {
+      //       images.addAll(data);
+      //       return refreshIndicator;
+      //     },
+      //     error: (error, stack) => Container(
+      //           color: Colors.red,
+      //         ),
+      //     loading: () => isRefresh ? CircularProgressIndicator() : refreshIndicator);
+      List<GalleryImage> list = ref.watch(newGalleryImagesProvider(Tuple2(safeId, width))).when(data: (data){
+        images.addAll(data);
+        return images;
+      }, error: (obj,stack){
+        return images;
+      }, loading: (){
+        return images;
+      });
+      listWidget = RefreshIndicator(
+        onRefresh: () async {
+          ref.read(galleryImagesProvider.state).state = [];
+          ref.read(galleryImagesPageProvider.state).state = 1;
+          return ref.refresh(newGalleryImagesProvider(Tuple2(safeId, width)));
+        },
+        child: GridView.builder(
+          padding: const EdgeInsets.fromLTRB(16, 0, 10, 0),
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            //横轴元素个数
+              crossAxisCount: 3,
+              // //纵轴间距
+              // mainAxisSpacing: 0.0,
+              // //横轴间距
+              // crossAxisSpacing: 0.0,
+              //子组件宽高长度比例
+              childAspectRatio: 1.0),
+          itemCount: list.length,
+          itemBuilder: (BuildContext context, int index) => GalleryImageView(list[index]),
+        ),
+      );
+    }
 
     return Scaffold(
       body: Container(
@@ -99,8 +143,7 @@ class _GalleryPageState extends ConsumerState<GalleryPage> {
                         ),
                         child: (Row(
                           children: [
-                            SvgPicture.asset('assets/images/icon_search.svg',
-                                height: 24, width: 24),
+                            SvgPicture.asset('assets/images/icon_search.svg', height: 24, width: 24),
                             const SizedBox(width: 10),
                             const Expanded(child: Text('search'))
                           ],
@@ -134,9 +177,10 @@ class _GalleryPageState extends ConsumerState<GalleryPage> {
                         // ref.read(galleryCategoriesProvider.state).state = []..addAll(categories);
 
                         // #3 dart把#2优化成了3个点的，叫spread，一个意思，语法糖
-                        ref.read(galleryCategoriesProvider.state).state = [
-                          ...categories
-                        ];
+
+                        ref.read(galleryImagesProvider.state).state = [];
+                        ref.read(galleryImagesPageProvider.state).state = 1;
+                        ref.read(galleryCategoriesProvider.state).state = [...categories];
 
                         // _controller.animateTo(125, duration: Duration(milliseconds: 250), curve: Curves.ease);
                       },
@@ -154,9 +198,17 @@ class _GalleryPageState extends ConsumerState<GalleryPage> {
             ],
           ),
         ),
-      ),floatingActionButton: FloatingActionButton(child: Icon(Icons.add),onPressed: (){
-        ref.read(galleryImagesPageProvider.state).state+=1;
-    },),floatingActionButtonLocation: FloatingActionButtonLocation.endTop,
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {
+          if (safeId != null) {
+            ref.read(galleryImagesPageProvider.state).state += 1;
+            // ref.refresh(newGalleryImagesProvider(Tuple2(safeId, width)));
+          }
+        },
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.endTop,
     );
   }
 }
@@ -177,8 +229,7 @@ class CategoryItemView extends ConsumerWidget {
         color: item.selected ? Colors.yellow : Colors.transparent,
       ),
       padding: const EdgeInsets.symmetric(vertical: 12, horizontal: 16),
-      child: Text(item.galleryCategory.name ?? "",
-          style: const TextStyle(fontSize: 12)),
+      child: Text(item.galleryCategory?.name ?? "", style: const TextStyle(fontSize: 12)),
     );
   }
 }
@@ -215,8 +266,7 @@ class GalleryImageView extends ConsumerWidget {
             width: 44,
             height: 44,
             child: Center(
-              child: SvgPicture.asset('assets/images/collection_unselected.svg',
-                  height: 27, width: 27),
+              child: SvgPicture.asset('assets/images/collection_unselected.svg', height: 27, width: 27),
             ),
           ),
           top: 0,
@@ -227,8 +277,7 @@ class GalleryImageView extends ConsumerWidget {
             width: 44,
             height: 44,
             child: Center(
-              child: SvgPicture.asset('assets/images/image_info.svg',
-                  height: 17, width: 17),
+              child: SvgPicture.asset('assets/images/image_info.svg', height: 17, width: 17),
             ),
           ),
           bottom: 0,
@@ -240,7 +289,7 @@ class GalleryImageView extends ConsumerWidget {
 }
 
 class GalleryCategoryItem {
-  final GalleryCategory galleryCategory;
+  final GalleryCategory? galleryCategory;
   bool selected;
 
   GalleryCategoryItem(this.galleryCategory, this.selected);
