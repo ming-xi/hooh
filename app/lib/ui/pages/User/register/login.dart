c2aa825c20d657fd47df2d4e79051549f831929b
muxue wei <752005116@qq.com>
2022-04-11 18:59:47 +0800

    注册页UI

--- app/lib/ui/pages/User/register/login.dart
+++ app/lib/ui/pages/User/register/login.dart
@@ -2,6 +2,7 @@ import 'package:app/ui/pages/User/register/sign_up.dart';
 import 'package:app/ui/pages/home/home.dart';
 import 'package:common/utils/preferences.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 class LoginScreen extends ConsumerStatefulWidget {
@@ -42,21 +43,18 @@ class _LoginPageState extends ConsumerState<LoginScreen> {
                 child: Center(
                   child: Column(
                     mainAxisSize: MainAxisSize.min,
-                    children: const [
-                      Text(
+                    children: [
+                      const Text(
                         'Let\'s start',
                         style: TextStyle(
-                          fontSize: 20,
-                          color: Colors.black,
+                          fontSize: 24,
+                          color: Color(0xFFF26218),
                         ),
                       ),
-                      Text(
-                        'HOOH',
-                        style: TextStyle(
-                          fontSize: 40,
-                          color: Colors.black,
-                        ),
+                      const SizedBox(
+                        height: 20,
                       ),
+                      SvgPicture.asset('assets/images/logo.svg', height: 160, width: 160)
                     ],
                   ),
                 ),
@@ -70,19 +68,30 @@ class _LoginPageState extends ConsumerState<LoginScreen> {
                   child: Column(
                     mainAxisSize: MainAxisSize.min,
                     children: [
-                      TextButton(onPressed: (){
-                        Navigator.push(
-                                      context,
-                                      MaterialPageRoute(
-                                          builder: (context) => SignUpScreen()));
-                      }, child: Text('Sign Up')),
-                      TextButton(onPressed: (){}, child: Text('Or Login')),
-                      TextButton(onPressed: (){
-                        String hasSkipped = "hasSkipped";
-                        preferences.putBool(hasSkipped, true);
-                        Navigator.pushReplacement(context,
-                            MaterialPageRoute(builder: (context) => HomeScreen()));
-                      }, child: Text('Skip')),
+                      TextButton(
+                          onPressed: () {
+                            Navigator.push(context, MaterialPageRoute(builder: (context) => SignUpScreen()));
+                          },
+                          style: flatBlackButtonStyle,
+                          child: const Text('Sign Up')),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      TextButton(
+                        onPressed: () {},
+                        child: const Text('Or Login'),
+                        style: flatWhiteButtonStyle,
+                      ),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      TextButton(
+                          onPressed: () {
+                            String hasSkipped = "hasSkipped";
+                            preferences.putBool(hasSkipped, true);
+                            Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => HomeScreen()));
+                          },
+                          child: Text('Skip', style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold, color: Colors.grey))),
                     ],
                   ),
                 ),
@@ -94,4 +103,22 @@ class _LoginPageState extends ConsumerState<LoginScreen> {
       ),
     );
   }
+
+  final ButtonStyle flatBlackButtonStyle = TextButton.styleFrom(
+      primary: Colors.white,
+      minimumSize: const Size(275, 64),
+      backgroundColor: Colors.black,
+      shape: const RoundedRectangleBorder(
+        borderRadius: BorderRadius.all(Radius.circular(22.0)),
+      ),
+      textStyle: TextStyle(fontSize: 20, fontWeight: FontWeight.bold));
+
+  final ButtonStyle flatWhiteButtonStyle = OutlinedButton.styleFrom(
+      primary: Colors.black,
+      shape: const RoundedRectangleBorder(
+        borderRadius: BorderRadius.all(Radius.circular(22.0)),
+      ),
+      minimumSize: const Size(275, 64),
+      side: BorderSide(width: 1, color: Colors.black),
+      textStyle: TextStyle(fontSize: 20, fontWeight: FontWeight.bold, color: Colors.black));
 }
