eb443800d06c351d06e6fb7106f0cbd4d4dfcdf3
xmk <289203664@qq.com>
2022-04-13 10:50:42 +0800

    修改注册流程

--- app/lib/ui/pages/home/me.dart
+++ app/lib/ui/pages/home/me.dart
@@ -29,7 +29,7 @@ class _InputPageState extends ConsumerState<MePage> {
                   // 正确的id：network.getUser("283bc4ee-e489-452f-9827-a15946cf9656")
 
                   //异步请求示例
-                  network.requestAsync<User>(network.getUser("4ee-e489-452f-9827-a15946cf9656"), (data) {
+                  network.requestAsync<User>(network.getUserInfo("4ee-e489-452f-9827-a15946cf9656"), (data) {
                     showDialog(
                         context: context,
                         builder: (e) => AlertDialog(
@@ -40,15 +40,15 @@ class _InputPageState extends ConsumerState<MePage> {
                     showDialog(
                         context: context,
                         builder: (context) {
-                          return  AlertDialog(
+                          return AlertDialog(
                             content: Text(error.message),
                           );
                         });
                   });
 
                   //合并future示例
-                  Future<User> response1 = network.getUser("4ee-e489-452f-9827-a15946cf9656");
-                  Future<User> response2 = network.getUser("4ee-e489-452f-9827-a15946cf9656");
+                  Future<User> response1 = network.getUserInfo("4ee-e489-452f-9827-a15946cf9656");
+                  Future<User> response2 = network.getUserInfo("4ee-e489-452f-9827-a15946cf9656");
                   Future.wait([response1, response2]).then((value) {
                     //所有的请求完成后会回调
                     var value1 = value[0];
@@ -68,9 +68,9 @@ class _InputPageState extends ConsumerState<MePage> {
                           content: Text("Loading..."),
                         );
                       });
-                  network.requestAsync<User>(network.getUser("283bc4ee-e489-452f-9827-a15946cf9656"), (data) async {
+                  network.requestAsync<User>(network.getUserInfo("283bc4ee-e489-452f-9827-a15946cf9656"), (data) async {
                     //如果要获取其他用户的数据，用同步请求。因为已经在一个异步代码块里面了
-                    User user = await network.getUser("283bc4ee-e489-452f-9827-a15946cf9656");
+                    User user = await network.getUserInfo("283bc4ee-e489-452f-9827-a15946cf9656");
                     //user.xxx 直接调用方法
                     //处理完后关闭对话框（方法和回退页面一样，pop掉最上层的东西——对话框）
                     Navigator.of(context).pop();
